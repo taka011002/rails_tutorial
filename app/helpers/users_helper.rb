ff7e31133aefe54408187977f8926b8bf74cae03
Piece123 <taka.rock.i4@gmail.com>
2018-03-28 15:23:15 +0900

    finish 10.3.2

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
 
   #引数で与えられたユーザーのGravatarの画像を返す
-  def gravatar_for(user)
+  def gravatar_for(user, options = { size: 80})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravater_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravater_url,alt: user.name, class: "gravatar")
