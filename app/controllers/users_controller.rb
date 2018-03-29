2842d791da6116fb6382de77fbd62bc2a2e42552
Piece123 <taka.rock.i4@gmail.com>
2018-03-29 18:28:43 +0900

    Finish 13.3.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,14 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only:[:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
-  
+
   def index
     @users = User.paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:ago])
   end
 
   def new
@@ -57,13 +58,7 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
+    
 
     def correct_user
      @user = User.find(params[:id])
